46a6db48ec437333c15fe3b37f6b73083bb505c2
nabaraj malla <nabarajmalla4000@gmail.com>
2024-07-15 10:10:29 +0545

    new file added

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   const user = req.cookies.uid;
-  console.log("user", user);
+  console.log("user hello", user);
   // if (!req.user) {
   //   console.log("user not found");
   //   return res.redirect("/staticRouter/login");
